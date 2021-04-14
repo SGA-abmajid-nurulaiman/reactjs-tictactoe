f9f5990f7ba3108980b8ba446a3194bc0e975f5c
5004123781 <NurulAiman.AbMajid@sony.com>
2021-04-14 13:53:15 +0800

    use 'state' props to update & draw X on clicked rect

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+constructor(props){
+    super(props);
+    this.state = {
+        value: null,
+    }
+}
+
     render() {
       return (
-        <button className="square" onClick={() => alert(`clicked`)}>
-          {this.props.value}
+        <button className="square"
+        onClick={() => {
+            this.setState({
+                value: 'X'
+            })
+            // alert(`next turn`)
+        } 
+        }>
+          {this.state.value}
         </button>
       );
     }
