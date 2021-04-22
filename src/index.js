c7c35c9f4d542293ee10962a2427eb8c5a4db1db
5004123781 <NurulAiman.AbMajid@sony.com>
2021-04-22 14:54:51 +0800

    change btn color when move is selected

--- src/index.js
+++ src/index.js
@@ -118,7 +118,7 @@ class Game extends React.Component {
             
             return(
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button className={this.state.stepNumber == move ? 'selected' : ''} onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             )
         })
